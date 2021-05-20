4aa1830ba8264483f2b5eef884743248cd3d9203
Jarrod Lilkendey <programmablewealth@gmail.com>
2021-05-20 19:41:43 +1000

    select pause button to unpause

--- src/tdgame/scenes/PausedScene.js
+++ src/tdgame/scenes/PausedScene.js
@@ -13,10 +13,16 @@ export class PausedScene extends Phaser.Scene {
     const _this = this;
     const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
     const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
-    this.pausedText = this.add.text(screenCenterX, screenCenterY, 'PAUSED (CLICK TO RESUME)', { font: '48px m5x7', fill: '#ffffff' }).setOrigin(0.5);
+    this.pausedText = this.add.text(screenCenterX, screenCenterY, 'PAUSED (CLICK BUTTON TO RESUME)', { font: '48px m5x7', fill: '#ffffff' }).setOrigin(0.5);
     this.pausedText.visible = true;
-    this.input.on('pointerdown', function (pointer) {
+
+    this.pausedSprite = this.add.sprite(88, 600, 'paused');
+    this.pausedSprite.setScale(0.8);
+    this.pausedSprite.setInteractive();
+
+    this.pausedSprite.on('pointerdown', function (pointer) {
       _this.pausedText.visible = false;
+      _this.pausedSprite.visible = false;
       _this.scene.resume(Constants.SCENES.GAMEPLAY);
     });
   }
