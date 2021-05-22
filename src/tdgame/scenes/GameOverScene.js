27c2a0802d2293673dda2d09a86cfead2ccf6ba7
Jarrod Lilkendey <programmablewealth@gmail.com>
2021-05-22 20:46:48 +1000

    show timing, gotchis placed and leaderboard rank on game over screen

--- src/tdgame/scenes/GameOverScene.js
+++ src/tdgame/scenes/GameOverScene.js
@@ -2,7 +2,9 @@ import Phaser from 'phaser';
 
 import { Constants } from '../Constants';
 
-import { writeScore, writeGotchiKills } from '../leaderboard/LeaderboardUtil';
+import { writeScore, writeGotchiKills, readScores } from '../leaderboard/LeaderboardUtil';
+
+const _ = require('lodash');
 
 export class GameOverScene extends Phaser.Scene {
   constructor(config) {
@@ -27,9 +29,15 @@ export class GameOverScene extends Phaser.Scene {
   create() {
     const _this = this;
     const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-    var gameOver = this.add.text(screenCenterX, 150, 'Game Over', { font: '128px m5x7', fill: '#ffffff' }).setOrigin(0.5);
-    var score = this.add.text(screenCenterX, 300, `Score: ${this.score}`, { font: '128px m5x7', fill: '#ffffff' }).setOrigin(0.5);
-    var button = this.add.sprite(400, 490, 'playagain').setInteractive();
+    var gameOver = this.add.text(screenCenterX, 75, 'Game Over', { font: '128px m5x7', fill: '#ffffff' }).setOrigin(0.5);
+    var score = this.add.text(screenCenterX, 160, `Score: ${this.score}`, { font: '80px m5x7', fill: '#ffffff' }).setOrigin(0.5);
+    var timeElapsed = this.add.text(screenCenterX, 215, `Time: ${this.timeElapsed}`, { font: '80px m5x7', fill: '#ffffff' }).setOrigin(0.5);
+    var gotchisPlaced = this.add.text(screenCenterX, 270, `Gotchis Placed: ${this.gotchisPlaced}`, { font: '80px m5x7', fill: '#ffffff' }).setOrigin(0.5);
+
+    var rank = this.add.text(screenCenterX, 325, 'Leaderboard Rank: loading...', { font: '80px m5x7', fill: '#ffffff' }).setOrigin(0.5).setInteractive();
+    rank.on('pointerdown', this.openLeaderboard, this);
+
+    var button = this.add.sprite(screenCenterX, 500, 'playagain').setOrigin(0.5).setInteractive();
     button.on('pointerdown', function (pointer) {
       console.log('pointerdown', pointer);
       // _this.musicSettings.music.stop();
@@ -37,7 +45,28 @@ export class GameOverScene extends Phaser.Scene {
       _this.scene.start(Constants.SCENES.LEVELSELECT, { musicSettings: _this.musicSettings });
     });
 
-    writeScore({ leaderboard: `leaderboard-level1-${this.gotchiCount}`, score: this.score, user: window.ethereum.selectedAddress, gotchisPlaced: this.gotchisPlaced, timeElapsed: this.timeElapsed });
+    writeScore(
+      { leaderboard: `leaderboard-level1-${this.gotchiCount}`, score: this.score, user: window.ethereum.selectedAddress, gotchisPlaced: this.gotchisPlaced, timeElapsed: this.timeElapsed }
+    ).then(() => {
+      readScores({ leaderboard: `leaderboard-level1-${_this.gotchiCount}` })
+        .then((results) => {
+          console.log('results', results);
+          let leaders = _.orderBy(results, ['score', 'timeElapsed', 'gotchisPlaced'], ['desc', 'asc', 'asc']);
+          let userRank = _.findIndex(leaders, ['user', window.ethereum.selectedAddress]) + 1;
+          rank.text = `Leaderboard Rank: ${userRank}`;
+        });
+    });
+
     writeGotchiKills({ gotchiKills: this.gotchiKills });
+  }
+
+  openLeaderboard() {
+    var url = 'https://aavegotchistats.com/tdleaderboard';
+    var s = window.open(url, '_blank');
+    if (s && s.focus) {
+      s.focus();
+    } else if (!s) {
+      window.location.href = url;
+    }
   }
 }
