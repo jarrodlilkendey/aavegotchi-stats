9e77e2c9ad711782d08f0dd05bac0ed70d5cfac2
Jarrod Lilkendey <programmablewealth@gmail.com>
2021-06-03 20:59:28 +1000

    hide navbar on tower defence page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -54,27 +54,32 @@ class NavBar extends Component {
   }
 
   render() {
-    return (
-      <div>
-      {this.renderSiteInfo()}
+    if (this.props.location.pathname !== "/td") {
+      return (
+        <div>
+        {this.renderSiteInfo()}
 
-      <nav className="navbar navbar-expand-lg navbar-dark">
-        <div className="container-fluid">
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-            <a className="navbar-brand" href="https://aavegotchistats.com">AavegotchiStats.com</a>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              {this.renderNavBar()}
-            </ul>
+        <nav className="navbar navbar-expand-lg navbar-dark">
+          <div className="container-fluid">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+              <a className="navbar-brand" href="https://aavegotchistats.com">AavegotchiStats.com</a>
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                {this.renderNavBar()}
+              </ul>
+            </div>
           </div>
+        </nav>
+
         </div>
-      </nav>
-
-
-      </div>
-    );
+      );
+    } else {
+      return(
+        <div></div>
+      );
+    }
   }
 }
 
