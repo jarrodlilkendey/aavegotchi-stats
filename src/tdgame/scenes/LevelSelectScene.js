479b53fb50d58fb65ec37b9328dfabce95663441
Jarrod Lilkendey <programmablewealth@gmail.com>
2021-06-03 20:59:11 +1000

    leaderboard completion flags on level select screen

--- src/tdgame/scenes/LevelSelectScene.js
+++ src/tdgame/scenes/LevelSelectScene.js
@@ -2,6 +2,10 @@ import Phaser from 'phaser';
 
 import { Constants } from '../Constants';
 
+import { readScores } from '../leaderboard/LeaderboardUtil';
+
+const _ = require('lodash');
+
 export class LevelSelectScene extends Phaser.Scene {
   constructor() {
     super({
@@ -18,10 +22,43 @@ export class LevelSelectScene extends Phaser.Scene {
     this.load.image('gotchis100', '/game/button_100gotchis.png');
     this.load.image('gotchis250', '/game/button_250gotchis.png');
     this.load.image('gotchis1000', '/game/button_1000gotchis.png');
+    this.load.image('complete', '/game/flagGreen.png');
   }
 
   create() {
     const _this = this;
+
+    readScores({ leaderboard: `leaderboard-level1-100` })
+      .then((results) => {
+        let leaders = _.orderBy(results, ['score', 'timeElapsed', 'gotchisPlaced'], ['desc', 'asc', 'asc']);
+        let userIndex = _.findIndex(leaders, ['user', window.ethereum.selectedAddress]);
+        if (userIndex != -1) {
+          if (leaders[userIndex].score == 100) {
+            _this.add.sprite(200, 520, 'complete').setScale(0.25);
+          }
+        }
+      });
+    readScores({ leaderboard: `leaderboard-level1-250` })
+        .then((results) => {
+          let leaders = _.orderBy(results, ['score', 'timeElapsed', 'gotchisPlaced'], ['desc', 'asc', 'asc']);
+          let userIndex = _.findIndex(leaders, ['user', window.ethereum.selectedAddress]);
+          if (userIndex != -1) {
+            if (leaders[userIndex].score == 250) {
+              _this.add.sprite(500, 520, 'complete').setScale(0.25);
+            }
+          }
+      });
+
+    readScores({ leaderboard: `leaderboard-level1-1000` })
+        .then((results) => {
+          let leaders = _.orderBy(results, ['score', 'timeElapsed', 'gotchisPlaced'], ['desc', 'asc', 'asc']);
+          let userIndex = _.findIndex(leaders, ['user', window.ethereum.selectedAddress]);
+          if (userIndex != -1) {
+            if (leaders[userIndex].score == 1000) {
+              _this.add.sprite(800, 520, 'complete').setScale(0.25);
+            }
+          }
+      });
 
     let title = this.add.text(this.game.config.width / 2, 80, `Select Level`, { font: '128px m5x7', fill: '#ffffff' }).setOrigin(0.5);
 
