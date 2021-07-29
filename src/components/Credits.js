70b36021a98a1fb701947d26d0ada4c55f9b5dac
Jarrod Lilkendey <programmablewealth@gmail.com>
2021-07-29 21:46:10 +1000

    change donation link to polygonscan instead of etherscan

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -19,7 +19,7 @@ class Credits extends Component {
       return(
         <nav className="navbar fixed-bottom navbar-dark" style={{'backgroundColor': '#2f3136'}}>
           <div className="container-fluid">
-            <p className="text-center" style={{'width': '100%', 'fontSize': '14px'}}><small className="text-muted"><a href='https://programmablewealth.com'>programmablewealth.com</a> | <a href='https://github.com/programmablewealth/aavegotchi-stats'>source code</a> | <a href='https://etherscan.io/address/0x708Ef16bF16Bb9f14CfE36075E9ae17bCd1C5B40'>donate</a></small></p>
+            <p className="text-center" style={{'width': '100%', 'fontSize': '14px'}}><small className="text-muted"><a href='https://programmablewealth.com'>programmablewealth.com</a> | <a href='https://github.com/programmablewealth/aavegotchi-stats'>source code</a> | <a href='https://polygonscan.com/address/0x708ef16bf16bb9f14cfe36075e9ae17bcd1c5b40'>donate</a></small></p>
           </div>
         </nav>
       );
