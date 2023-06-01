401652f8d5e550072dffdce7227da57cbdce5e7a
Jarrod Lilkendey <programmablewealth@gmail.com>
2023-06-01 21:17:52 +1000

    remove comments

--- src/util/MaticClient.js
+++ src/util/MaticClient.js
@@ -1,28 +1,9 @@
-// import WalletConnectProvider from "@maticnetwork/walletconnect-provider";
-// import WalletConnectProvider from "@walletconnect/web3-provider";
-
 const MaticPOSClient = require('@maticnetwork/maticjs').MaticPOSClient;
 
 export const connectToMatic = () => {
   const apiKeys = JSON.parse(process.env.REACT_APP_RPC_CONFIG);
   const parentProvider = `https://mainnet.infura.io/v3/${apiKeys.infura}`;
   const maticProvider = `https://rpc-mainnet.maticvigil.com/v1/${apiKeys.maticvigil}`;
-  // const maticProvider = `https://polygon-mainnet.infura.io/v3/${apiKeys.infura}`;
-
-  // const maticProvider = new WalletConnectProvider({
-  //   infuraId: `${apiKeys.infura}`,
-  //   // host: `https://rpc-mainnet.maticvigil.com/v1/${apiKeys.maticvigil}`,
-  //   // callbacks: {
-  //   //   onConnect: () => {
-  //   //     console.log('walletconnect onConnect');
-  //   //   },
-  //   //   onDisconnect: () => {
-  //   //     console.log('walletconnect onDisconnect');
-  //   //   }
-  //   // }
-  // });
-
-  // maticProvider.enable();
 
   const maticPOSClient = new MaticPOSClient({
     network: "mainnet",
